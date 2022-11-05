f0a9c95eb44447e13c3fdaea9516f75fe94fde80
gennadytabala <45870843+gennadytabala@users.noreply.github.com>
2022-11-05 21:28:47 +0200

    app update and update notification done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,16 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HeaderComponent } from './header/header.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherComponent
+    WeatherComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +28,9 @@ import { environment } from '../environments/environment';
     MatIconModule,
     MatInputModule,
     MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSnackBarModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
